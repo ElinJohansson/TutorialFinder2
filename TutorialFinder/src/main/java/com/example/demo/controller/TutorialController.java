2931575c01acfd11d584bcc24127334fbd6d58ad
Elin Johansson <elin.johansson42@gmail.com>
2018-04-10 14:31:13 +0200

    Add Repository Interface and implemented on TutorialRepo

--- TutorialFinder/src/main/java/com/example/demo/controller/TutorialController.java
+++ TutorialFinder/src/main/java/com/example/demo/controller/TutorialController.java
@@ -1,9 +1,12 @@
 package com.example.demo.controller;
 
+import com.example.demo.repository.Repository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 
 @Controller
 public class TutorialController {
 
-
+    @Autowired
+    private Repository repository;
 }

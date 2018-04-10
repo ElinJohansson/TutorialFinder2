9b1f30d02f153acfee1c8b33e2e45ab0658c0805
Elin Johansson <elin.johansson42@gmail.com>
2018-04-10 15:00:48 +0200

    Add exception class and controller methods

--- TutorialFinder/src/main/java/com/example/demo/repository/Repository.java
+++ TutorialFinder/src/main/java/com/example/demo/repository/Repository.java
@@ -1,4 +1,8 @@
 package com.example.demo.repository;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
 public interface Repository {
+    void createTutorial(String title, String descr, LocalDateTime creationDate, String language, String format, String url, List<String> tags);
 }
